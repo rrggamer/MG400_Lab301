63da192a18e6e9a7e02dd321ccd39ad8b45af987
IXVIA <phakaewpra@gmail.com>
2025-12-02 12:22:36 +0700

    Drawing Done

---
+++ drawing.lua
@@ -0,0 +1,46 @@
+-- Version: Lua 5.3.5
+-- This thread is the main thread and can call any commands.
+
+-- Drawing Lab1
+--First Drawing
+MovL(P6)
+MovL(P1)
+MovL(P2)
+MovL(P3)
+MovL(P4)
+MovL(P5)
+--Second Drawing
+MovL(P7)
+MovL(P8)
+Arc(P9, P10)
+MovL(P11)
+MovL(P8)
+MovL(P12)
+--Third Drawing
+MovL(P13)
+MovL(P14)
+Circle(P15, P16, 1)
+MovL(P18)
+--Fourth Drawing
+MovL(P19)
+MovL(P20)
+MovL(P21)
+MovL(P22)
+MovL(P23)
+MovL(P24)
+MovL(P25)
+MovL(P26)
+MovL(P27)
+MovL(P28)
+MovL(P29)
+MovL(P30)
+--Fifth Drawing
+MovL(P31)
+MovL(P32)
+MovL(P33)
+MovL(P34)
+MovL(P35)
+MovL(P36)
+
+
+
